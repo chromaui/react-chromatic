411c3c601b5868e70a7369a6c498080c8540150a
Chroma Machine <machine@hichroma.com>
2017-11-06 00:07:48 +0000

    Commit 38be0edf074c28eca9dbd6d0915684d75a6e35d1

--- dist/assets/runtimes/storybook-v2.js
+++ dist/assets/runtimes/storybook-v2.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _react = require('@storybook/react');
+var _storybook = require('@kadira/storybook');
 
 var _storybookCommon = require('./storybook-common');
 
@@ -13,6 +13,6 @@ var _storybookCommon2 = _interopRequireDefault(_storybookCommon);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // XXX: Should we make this a peer dependency?
-// eslint-disable-next-line import/no-extraneous-dependencies
-exports.default = (0, _storybookCommon2.default)(_react.getStorybook);
-//# sourceMappingURL=storybook.js.map
+// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
+exports.default = (0, _storybookCommon2.default)(_storybook.getStorybook);
+//# sourceMappingURL=storybook-v2.js.map
