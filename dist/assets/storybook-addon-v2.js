13039e2dc0000ae78b4b34e69d1081723fb8d749
Chroma Machine <machine@hichroma.com>
2017-11-08 06:27:56 +0000

    Commit ffd5060d1b4788d72957db437cdc95e761faa0ae

---
+++ dist/assets/storybook-addon-v2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var _reactChromatic = require('react-chromatic');
+
+var _reactChromatic2 = _interopRequireDefault(_reactChromatic);
+
+var _storybookV = require('react-chromatic/storybook-v2');
+
+var _storybookV2 = _interopRequireDefault(_storybookV);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/* eslint-disable import/no-extraneous-dependencies,import/no-unresolved,import/extensions */
+(0, _reactChromatic2.default)({
+  noWidget: true,
+  runtimes: [_storybookV2.default]
+});
+//# sourceMappingURL=storybook-addon-v2.js.map
