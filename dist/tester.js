61a22f299ecc6fd6d80f9b755cc9f60026604ae2
Chroma Machine <machine@hichroma.com>
2018-01-30 05:20:57 +0000

    Commit d114ef4fabc3e9de81ee1bebbd271e774e265ab7

--- dist/tester.js
+++ dist/tester.js
@@ -93,58 +93,36 @@ module.exports = require("denodeify");
 
 /***/ }),
 /* 4 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-// Note this file differs from our usual convention because it is packaged
-var _process$env = process.env,
-    _process$env$CHROMATI = _process$env.CHROMATIC_SERVER_PORT,
-    CHROMATIC_SERVER_PORT = _process$env$CHROMATI === undefined ? 3004 : _process$env$CHROMATI,
-    _process$env$CHROMATI2 = _process$env.CHROMATIC_INDEX_URL,
-    CHROMATIC_INDEX_URL = _process$env$CHROMATI2 === undefined ? 'https://index.chromaticqa.com' : _process$env$CHROMATI2,
-    _process$env$CHROMATI3 = _process$env.CHROMATIC_TUNNEL_URL,
-    CHROMATIC_TUNNEL_URL = _process$env$CHROMATI3 === undefined ? 'https://tunnel.chromaticqa.com' : _process$env$CHROMATI3;
-exports.CHROMATIC_SERVER_PORT = CHROMATIC_SERVER_PORT;
-exports.CHROMATIC_INDEX_URL = CHROMATIC_INDEX_URL;
-exports.CHROMATIC_TUNNEL_URL = CHROMATIC_TUNNEL_URL;
-
-/***/ }),
-/* 5 */
 /***/ (function(module, exports) {
 
 module.exports = require("debug");
 
 /***/ }),
-/* 6 */
+/* 5 */
 /***/ (function(module, exports) {
 
 module.exports = require("babel-runtime/helpers/classCallCheck");
 
 /***/ }),
-/* 7 */
+/* 6 */
 /***/ (function(module, exports) {
 
 module.exports = require("babel-runtime/helpers/createClass");
 
 /***/ }),
-/* 8 */
+/* 7 */
 /***/ (function(module, exports) {
 
 module.exports = require("babel-runtime/helpers/extends");
 
 /***/ }),
-/* 9 */
+/* 8 */
 /***/ (function(module, exports) {
 
 module.exports = require("child_process");
 
 /***/ }),
-/* 10 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -162,15 +140,15 @@ var _asyncToGenerator2 = __webpack_require__(0);
 
 var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
-var _extends2 = __webpack_require__(8);
+var _extends2 = __webpack_require__(7);
 
 var _extends3 = _interopRequireDefault(_extends2);
 
-var _classCallCheck2 = __webpack_require__(6);
+var _classCallCheck2 = __webpack_require__(5);
 
 var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
 
-var _createClass2 = __webpack_require__(7);
+var _createClass2 = __webpack_require__(6);
 
 var _createClass3 = _interopRequireDefault(_createClass2);
 
@@ -283,6 +261,33 @@ var GraphQLClient = function () {
 exports.default = GraphQLClient;
 
 /***/ }),
+/* 10 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+// Note this file differs from our usual convention because it is packaged
+var _process$env = process.env,
+    _process$env$CHROMATI = _process$env.CHROMATIC_SERVER_PORT,
+    CHROMATIC_SERVER_PORT = _process$env$CHROMATI === undefined ? 3004 : _process$env$CHROMATI,
+    _process$env$CHROMATI2 = _process$env.CHROMATIC_INDEX_URL,
+    CHROMATIC_INDEX_URL = _process$env$CHROMATI2 === undefined ? 'https://index.chromaticqa.com' : _process$env$CHROMATI2,
+    _process$env$CHROMATI3 = _process$env.CHROMATIC_TUNNEL_URL,
+    CHROMATIC_TUNNEL_URL = _process$env$CHROMATI3 === undefined ? 'https://tunnel.chromaticqa.com' : _process$env$CHROMATI3,
+    _process$env$CHROMATI4 = _process$env.CHROMATIC_CREATE_TUNNEL,
+    CHROMATIC_CREATE_TUNNEL = _process$env$CHROMATI4 === undefined ? 'true' : _process$env$CHROMATI4,
+    CHROMATIC_APP_CODE = _process$env.CHROMATIC_APP_CODE;
+exports.CHROMATIC_SERVER_PORT = CHROMATIC_SERVER_PORT;
+exports.CHROMATIC_INDEX_URL = CHROMATIC_INDEX_URL;
+exports.CHROMATIC_TUNNEL_URL = CHROMATIC_TUNNEL_URL;
+exports.CHROMATIC_CREATE_TUNNEL = CHROMATIC_CREATE_TUNNEL;
+exports.CHROMATIC_APP_CODE = CHROMATIC_APP_CODE;
+
+/***/ }),
 /* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -309,13 +314,12 @@ var _denodeify = __webpack_require__(3);
 
 var _denodeify2 = _interopRequireDefault(_denodeify);
 
-var _environment = __webpack_require__(4);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function () {
   var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(_ref) {
-    var port = _ref.port;
+    var tunnelUrl = _ref.tunnelUrl,
+        port = _ref.port;
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
@@ -330,7 +334,7 @@ exports.default = function () {
           case 2:
             return _context.abrupt('return', (0, _denodeify2.default)(_localtunnel2.default)(port, {
               local_host: 'localhost',
-              host: _environment.CHROMATIC_TUNNEL_URL
+              host: tunnelUrl
             }));
 
           case 3:
@@ -668,13 +672,13 @@ var getBaselineCommits = exports.getBaselineCommits = function () {
   };
 }();
 
-var _child_process = __webpack_require__(9);
+var _child_process = __webpack_require__(8);
 
 var _denodeify = __webpack_require__(3);
 
 var _denodeify2 = _interopRequireDefault(_denodeify);
 
-var _debug = __webpack_require__(5);
+var _debug = __webpack_require__(4);
 
 var _debug2 = _interopRequireDefault(_debug);
 
@@ -771,11 +775,11 @@ var _asyncToGenerator2 = __webpack_require__(0);
 
 var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
-var _classCallCheck2 = __webpack_require__(6);
+var _classCallCheck2 = __webpack_require__(5);
 
 var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
 
-var _createClass2 = __webpack_require__(7);
+var _createClass2 = __webpack_require__(6);
 
 var _createClass3 = _interopRequireDefault(_createClass2);
 
@@ -980,7 +984,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.checkResponse = undefined;
 
-var _extends2 = __webpack_require__(8);
+var _extends2 = __webpack_require__(7);
 
 var _extends3 = _interopRequireDefault(_extends2);
 
@@ -1115,7 +1119,7 @@ var waitForResponse = function () {
   };
 }();
 
-var _child_process = __webpack_require__(9);
+var _child_process = __webpack_require__(8);
 
 var _isomorphicFetch = __webpack_require__(26);
 
@@ -1294,15 +1298,13 @@ var _denodeify2 = _interopRequireDefault(_denodeify);
 
 var _nodeAsk = __webpack_require__(18);
 
-var _debug = __webpack_require__(5);
+var _debug = __webpack_require__(4);
 
 var _debug2 = _interopRequireDefault(_debug);
 
 var _treeKill = __webpack_require__(19);
 
 var _treeKill2 = _interopRequireDefault(_treeKill);
-
-var _environment = __webpack_require__(4);
 
 var _runtimes = __webpack_require__(14);
 
@@ -1318,13 +1320,15 @@ var _tunnel2 = _interopRequireDefault(_tunnel);
 
 var _packageJson = __webpack_require__(13);
 
-var _GraphQLClient = __webpack_require__(10);
+var _GraphQLClient = __webpack_require__(9);
 
 var _GraphQLClient2 = _interopRequireDefault(_GraphQLClient);
 
 var _git = __webpack_require__(12);
 
 var _package = __webpack_require__(16);
+
+var _environment = __webpack_require__(10);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -1366,6 +1370,8 @@ exports.default = function () {
         verbose = _ref3$verbose === undefined ? false : _ref3$verbose,
         _ref3$indexUrl = _ref3.indexUrl,
         indexUrl = _ref3$indexUrl === undefined ? _environment.CHROMATIC_INDEX_URL : _ref3$indexUrl,
+        _ref3$tunnelUrl = _ref3.tunnelUrl,
+        tunnelUrl = _ref3$tunnelUrl === undefined ? _environment.CHROMATIC_TUNNEL_URL : _ref3$tunnelUrl,
         _ref3$createTunnel = _ref3.createTunnel,
         createTunnel = _ref3$createTunnel === undefined ? true : _ref3$createTunnel,
         _ref3$originalArgv = _ref3.originalArgv,
@@ -1494,7 +1500,7 @@ exports.default = function () {
 
             log('Opening tunnel to Chromatic capture servers');
             _context2.next = 54;
-            return (0, _tunnel2.default)({ port: port });
+            return (0, _tunnel2.default)({ tunnelUrl: tunnelUrl, port: port });
 
           case 54:
             tunnel = _context2.sent;
